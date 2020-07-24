d498f35d9c890ec842941fa521950ddc6a072cf7
Malson-Lansom <61781906+Malson-Lansom@users.noreply.github.com>
2020-07-24 16:09:06 +0900

    サインイン入力画面の作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,55 @@
+class UsersController < ApplicationController
+  # デバイスで処理できない登録部分などをまとめていく
+
+  # 他テーブルとの兼ね合いもあり、ルーティングとアソシエーションネスト完成後に有効にしていく
+  # before_action :set_user, only: [:show, :edit, :profile, :profile_update, :logout]
+  # before_action :set_address, only: [:edit, :update]
+
+  def show
+  end
+
+  def edit
+  end
+
+  # def update
+  #   if @address.update(address_params)
+  #     redirect_to edit_user_path(@address)
+  #   else
+  #     render :edit
+  #   end
+  # end
+
+  # サインアップのセッションで情報を入力する段階を踏むprofileと編集用のupdateを定義します
+  def profile
+  end
+
+  def profile_update
+    if @user.update(user_params)
+      redirect_to user_path
+    else
+      render :profile
+    end
+  end
+
+  def logout
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:nickname)
+  end
+
+  def set_user
+    @user.User.find(params[:id])
+  end
+
+  # postalテーブルが必要
+  # def address_params
+  #   params.require(:address).permit(:postal_code, :prefecture, :city, :address, :apartment)
+  # end
+
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'products#index'
+
+  resources :users, only: [:show, :edit, :update] do
+    member do
+      get 'profile'           # profile_user_path         サインアップセッション１
+      patch 'profile_update'  # profile_update_user_path  
+      get 'logout'            # logout_user_path          
+    end
+  end
+
   resources :products
+
+
 end
