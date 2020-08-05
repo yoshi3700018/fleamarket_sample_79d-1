67404575c83a86aa69c214b05f16ced3768bde90
kazunari0220 <67147883+kazunari0220@users.noreply.github.com>
2020-08-05 17:47:57 +0900

    ファイル修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -40,14 +40,14 @@ class ProductsController < ApplicationController
   def destroy
   end
 
-  def confirm
-    card = Card.find_by(user_id: current_user.id)
-    if card
-      Payjp.api_key = Rails.application.credentials.development[:PAYJP_PRIVATE_KEY]
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      @default_card_information = customer.cards.retrieve(card.card_id)
-    end
-  end
+  # def confirm
+  #   card = Card.find_by(user_id: current_user.id)
+  #   if card
+  #     Payjp.api_key = Rails.application.credentials.development[:PAYJP_PRIVATE_KEY]
+  #     customer = Payjp::Customer.retrieve(card.customer_id)
+  #     @default_card_information = customer.cards.retrieve(card.card_id)
+  #   end
+  # end
 
   def top
   end
