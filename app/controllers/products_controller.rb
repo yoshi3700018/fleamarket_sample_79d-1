185b88b542990e2bc9bfa7ec2b3debc002a20a86
yoshi3700018 <yoshiyuki.hashimoto.0@gmail.com>
2020-08-05 19:10:33 +0900

    product_controller_done

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -65,7 +65,7 @@ class ProductsController < ApplicationController
         :category_id, :brand_id, 
         :shipping_status, :deliver, 
         :prefecture, :shipping_dates, 
-        :price, :users_id, 
+        :price,
         images_attributes: [:image]).merge(user_id: current_user.id)
   end
   # いいね機能を取り扱った福本さんの方とパラメータの定義が異なる可能性、ひとまずSHOW画面に表示させるための定義、マージ時確認
