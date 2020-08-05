a72d94583e45a8a876ed4b2f940464c4e167875d
yoshi3700018 <yoshiyuki.hashimoto.0@gmail.com>
2020-08-05 14:15:20 +0900

    index_haml_image

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
 
-  belongs_to :user, optional: true
+  belongs_to :user
   belongs_to :category
   has_many :images, inverse_of: :product
   accepts_nested_attributes_for :images, allow_destroy: true
