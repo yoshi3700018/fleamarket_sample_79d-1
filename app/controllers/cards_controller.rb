3cf424532d16d1b7261971fb5da59f6e0c866c27
Malson-Lansom <61781906+Malson-Lansom@users.noreply.github.com>
2020-08-10 17:51:09 +0900

    payjp

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -8,7 +8,7 @@ class CardsController < ApplicationController
 
 
   def pay #payjpとCardのデータベース作成
-    Payjp.api_key = ENV[:PAYJP_PRIVATE_KEY]
+    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     #保管した顧客IDでpayjpから情報取得
     if params['payjp_token'].blank?
       redirect_to new_card_path
@@ -28,7 +28,7 @@ class CardsController < ApplicationController
 
   def destroy #PayjpとCardデータベースを削除
     if @card.present?
-      Payjp.api_key = ENV[:PAYJP_PRIVATE_KEY]
+      Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
       customer = Payjp::Customer.retrieve(@card.customer_id)
       customer.delete
       @card.delete
@@ -40,7 +40,7 @@ class CardsController < ApplicationController
     if @card.blank?
       redirect_to new_card_path 
     else
-      Payjp.api_key = ENV[:PAYJP_PRIVATE_KEY]
+      Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
       customer = Payjp::Customer.retrieve(@card.customer_id)
       @default_card_information = customer.cards.retrieve(@card.card_id)
     end
