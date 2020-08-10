619e73b0ad7a26316a3986947c9ed055fd942919
Malson-Lansom <61781906+Malson-Lansom@users.noreply.github.com>
2020-08-10 17:45:58 +0900

    Update cards_controller.rb

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -28,7 +28,7 @@ class CardsController < ApplicationController
 
   def destroy #PayjpとCardデータベースを削除
     if @card.present?
-      Payjp.api_key = Rails.application.credentials[:PAYJP_PRIVATE_KEY]
+      Payjp.api_key = ENV[:PAYJP_PRIVATE_KEY]
       customer = Payjp::Customer.retrieve(@card.customer_id)
       customer.delete
       @card.delete
@@ -40,7 +40,7 @@ class CardsController < ApplicationController
     if @card.blank?
       redirect_to new_card_path 
     else
-      Payjp.api_key = Rails.application.credentials[:PAYJP_PRIVATE_KEY]
+      Payjp.api_key = ENV[:PAYJP_PRIVATE_KEY]
       customer = Payjp::Customer.retrieve(@card.customer_id)
       @default_card_information = customer.cards.retrieve(@card.card_id)
     end
