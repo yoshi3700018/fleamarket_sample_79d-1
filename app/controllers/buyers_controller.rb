a648967fdb50c96f5ef5f14c2b24e37698687b9d
Malson-Lansom <61781906+Malson-Lansom@users.noreply.github.com>
2020-08-11 11:43:32 +0900

    PAYJP3

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -7,7 +7,8 @@ class BuyersController < ApplicationController
       #登録された情報がない場合にカード登録画面に移動
       redirect_to new_card_path
     else
-      Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+      Payjp.api_key = Rails.application.credentials.payjp[:PAYJP_PRIVATE_KEY]
+      # Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
       #保管した顧客IDでpayjpから情報取得
       customer = Payjp::Customer.retrieve(@card.customer_id) 
       #カード情報表示のためインスタンス変数に代入
@@ -16,7 +17,8 @@ class BuyersController < ApplicationController
   end
 
   def pay
-    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+    Payjp.api_key = Rails.application.credentials.payjp[:PAYJP_PRIVATE_KEY]
+    # Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     Payjp::Charge.create(
       :amount => @product.price, #支払金額を引っ張ってくる
       :customer => @card.customer_id,  #顧客ID
