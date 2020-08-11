ba9a700888b8e32c84e943e26d4fe553d7b95406
Malson-Lansom <61781906+Malson-Lansom@users.noreply.github.com>
2020-08-11 11:49:59 +0900

    payjp4

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -8,7 +8,8 @@ class CardsController < ApplicationController
 
 
   def pay #payjpとCardのデータベース作成
-    Payjp.api_key = Rails.application.credentials.payjp[:PAYJP_PRIVATE_KEY]
+    Rails.application.credentials.dig(:payjp, :PAYJP_PRIVATE_KEY)
+    # Payjp.api_key = Rails.application.credentials.payjp[:PAYJP_PRIVATE_KEY]
     # Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     #保管した顧客IDでpayjpから情報取得
     if params['payjp_token'].blank?
@@ -29,7 +30,8 @@ class CardsController < ApplicationController
 
   def destroy #PayjpとCardデータベースを削除
     if @card.present?
-      Payjp.api_key = Rails.application.credentials.payjp[:PAYJP_PRIVATE_KEY]
+      Rails.application.credentials.dig(:payjp, :PAYJP_PRIVATE_KEY)
+      # Payjp.api_key = Rails.application.credentials.payjp[:PAYJP_PRIVATE_KEY]
       # Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
       customer = Payjp::Customer.retrieve(@card.customer_id)
       customer.delete
@@ -42,7 +44,8 @@ class CardsController < ApplicationController
     if @card.blank?
       redirect_to new_card_path 
     else
-      Payjp.api_key = Rails.application.credentials.payjp[:PAYJP_PRIVATE_KEY]
+      Rails.application.credentials.dig(:payjp, :PAYJP_PRIVATE_KEY)
+      # Payjp.api_key = Rails.application.credentials.payjp[:PAYJP_PRIVATE_KEY]
       # Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
       customer = Payjp::Customer.retrieve(@card.customer_id)
       @default_card_information = customer.cards.retrieve(@card.card_id)
